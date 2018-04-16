052ea369510aaa76342d0fd48d521f0a736b92aa
MelaJia <838672863@qq.com>
2018-04-16 15:34:40 +0800

    登录

--- app/src/main/java/cn/edu/gdmec/android/sharepreferencestest/FileSaveLogin.java
+++ app/src/main/java/cn/edu/gdmec/android/sharepreferencestest/FileSaveLogin.java
@@ -1,6 +1,7 @@
 package cn.edu.gdmec.android.sharepreferencestest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 
 import java.util.Map;
 /**
- * 将用户名和密码保存在文件中
+ * 将用户名和密码保存在私有目录中
  * 缺点：第一次保存用户名和密码后
  * */
 
@@ -23,14 +24,16 @@ public class FileSaveLogin extends Activity implements View.OnClickListener {
     private EditText et_password;
     private CheckBox renumber_pas;
     private Button login;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initView();
+        mContext=this;
         //取出之前保存的用户名和密码(回显）
-        Map<String ,String> map=UserInfoUtil.getUserInfo();//获取用户名密码；
+        Map<String ,String> map=UserInfoUtil.getUserInfo(mContext);//获取用户名密码；
         if (map!=null){
             String username=map.get("username");
             String password=map.get("password");
@@ -60,7 +63,7 @@ public class FileSaveLogin extends Activity implements View.OnClickListener {
                 String userInfo=userName+"##"+password;
                 //2、判断是否选择保存密码
                 if (isSave){
-                    boolean result=UserInfoUtil.saveUserInfo(userInfo);
+                    boolean result=UserInfoUtil.saveUserInfo(mContext,userInfo);
                     if (result){
                         Toast.makeText(this, "已保存好用户名和密码，记住密码", Toast.LENGTH_SHORT).show();
                     }else{
--- app/src/main/java/cn/edu/gdmec/android/sharepreferencestest/UserInfoUtil.java
+++ app/src/main/java/cn/edu/gdmec/android/sharepreferencestest/UserInfoUtil.java
@@ -1,5 +1,7 @@
 package cn.edu.gdmec.android.sharepreferencestest;
 
+import android.content.Context;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,9 +16,13 @@ import java.util.Map;
  */
 
 public class UserInfoUtil {
-    public static boolean saveUserInfo(String userInfo){
-        try{
-            File file=new File("/data/data/cn.edu.gdmec.android.sharepreferencestest/","a.txt");
+    public static boolean saveUserInfo(Context context,String userInfo){
+       // String userInfo=userName+"##"+password;
+            try{
+         //   String path="/data/data/cn.edu.gdmec.android.sharepreferencestest/";
+            //通过Context对象获取私有目录路径
+            String path=context.getFilesDir().getPath();
+            File file=new File(path,"a.txt");
             FileOutputStream fos= new FileOutputStream(file,true);
             fos.write(userInfo.getBytes());
 
@@ -30,9 +36,10 @@ public class UserInfoUtil {
           return true;
     }
     //封装用户名和密码
-    public static Map<String ,String> getUserInfo(){
+    public static Map<String ,String> getUserInfo(Context context){
         try{
-            File file=new File("/data/data/cn.edu.gdmec.android.sharepreferencestest/","a.txt");
+            String path=context.getFilesDir().getPath();
+            File file=new File(path,"a.txt");
             FileInputStream fis= new FileInputStream(file);
             BufferedReader br=new BufferedReader(new InputStreamReader(fis));
             String[] info=br.readLine().split("##");
